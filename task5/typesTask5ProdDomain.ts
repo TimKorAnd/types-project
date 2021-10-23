af4fabdfffa87b36239a232926b14840fbcd816d
TimKorAnd <timkorand@gmail.com>
2021-10-23 19:48:53 +0300

    solved by author

--- task5/typesTask5ProdDomain.ts
+++ task5/typesTask5ProdDomain.ts
@@ -3,9 +3,14 @@ type Org = string;
 type ProdDomain<O extends Org> = `www.${O}.some-service.com`;
 type StageDomain<O extends Org> = `www.${O}.some-service-stage.com`;
 
-type ProdDomainToStageDomain = <D extends string>(prodDomain: D) => unknown;
+type ProdDomainToStageDomain1 = <O extends string>(
+  prodDomain: ProdDomain<O>
+) => StageDomain<O>;
+type ProdDomainToStageDomain2 = <D extends string>(
+  prodDomain: D
+) => D extends ProdDomain<infer O> ? StageDomain<O> : never;
 
-const prodDomainToStageDomain: ProdDomainToStageDomain = (prodDomain) => {};
+declare const prodDomainToStageDomain: ProdDomainToStageDomain2;
 
 const googleProdDomain = "www.google.some-service.com";
 const appleProdDomain = "www.apple.some-service.com";
@@ -14,3 +19,4 @@ const facebookProdDomain = "www.facebook.some-service.com";
 const googleStageDomain = prodDomainToStageDomain(googleProdDomain); // 'www.google.some-service-stage.com';
 const appleStageDomain = prodDomainToStageDomain(appleProdDomain); // 'www.apple.some-service-stage.com';
 const facebookStageDomain = prodDomainToStageDomain(facebookProdDomain); // 'www.facebook.some-service-stage.com';
+const facebookStageDomaina = prodDomainToStageDomain("a"); // 'www.facebook.some-service-stage.com';
